ac6d2d532b3e605ebcbf5038a11c74ecdfa5ee3b
kulvir sekhon <kulvir sekhon>
2017-07-17 13:38:36 -0700

    Adding ClientVariables

---
+++ ClientVariables.py
@@ -0,0 +1,9 @@
+class ClientVariables():
+	alias = None
+	address = None
+	chatroom = None
+	def __init__(self,alias,address,chatroom):
+		self.alias = alias
+		self.address = address
+		self.chatroom = chatroom
+		
